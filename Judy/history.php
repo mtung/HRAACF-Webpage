853ca9aeae05fdeb180e809e30e3928005f36a99
Judy Park <judypark@dhcp-10-249-25-38.fas.harvard.edu>
2011-12-05 15:12:23 -0500

    I'll actually push this time

--- Judy/history.php
+++ Judy/history.php
@@ -3,12 +3,12 @@
 	<head>
 		<title>Harvard-Radcliffe Asian American Christian Fellowship</title>
 		<link rel="stylesheet" href="CSS/style2.css"> 
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-		<script src="jquery-1.2.1.min.js" type="text/javascript"></script>
-		<script src="menu.js" type="text/javascript"></script>
-		<script src="jquery.cookie.js" type="text/javascript"></script>
 		<link href='http://fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Antic' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+		<script src="scripts/jquery-1.2.1.min.js" type="text/javascript"></script>
+		<script src="scripts/menu.js" type="text/javascript"></script>
+		<script src="scripts/jquery.cookie.js" type="text/javascript"></script>
 	</head> 
 		<body> 
 			<div id="top"> 
@@ -26,8 +26,8 @@
 						<h1>History</h1> 
 						<p> Organized in the fall of 1994, HRAACF is the sister InterVarsity chapter of the Harvard-Radcliffe Christian Fellowship, which pioneered HRAACF in order to pursue its goals of engaging the Harvard campus and better understanding the value of multiethnicity. HRAACF offers Asian-specific support and cultural lenses to living out the Gospel. It strives to reach out to Asian Americans at Harvard and to develop leaders for the Asian American church.</p>  
 						<p>In 2010 the Harvard College Black Christian Fellowship was organized out of HRCF for similar reasons. Now HRCF, HRAACF, HCBCF work under a covenant relationship that provides regular joint Large Group meetings, joint retreats, cooperative campus-wide evangelism events, and partnering staff teams. HRCF, HRAACF, HCBCF renew their convenant annually at their week-long retreat at the end of the year.</p> 
+					</div> 
 				</div> 
-			</div> 
 				
 			<div id="bottom"> 
 				<?php include('includes/footer.php');?>
--- Judy/index.php
+++ Judy/index.php
@@ -3,13 +3,12 @@
 	<head>
 		<title>Harvard-Radcliffe Asian American Christian Fellowship</title>
 		<link rel="stylesheet" href="CSS/style2.css"> 
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-		<script src="jquery-1.2.1.min.js" type="text/javascript"></script>
-		<script src="menu.js" type="text/javascript"></script>
-		<script src="jquery.cookie.js" type="text/javascript"></script>
 		<link href='http://fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Antic' rel='stylesheet' type='text/css'>
-		
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+		<script src="scripts/jquery-1.2.1.min.js" type="text/javascript"></script>
+		<script src="scripts/menu.js" type="text/javascript"></script>
+		<script src="scripts/jquery.cookie.js" type="text/javascript"></script>
 	</head> 
 		<body> 
 			<div id="top"> 
