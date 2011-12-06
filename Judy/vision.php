22d0f9c8bee171fa556466cf19537bf9445b247b
Judy Park <judypark@judy-parks-macbook-pro.local>
2011-12-06 18:52:26 -0500

    edited user nav

--- Judy/vision.php
+++ Judy/vision.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html>
 <html>	
 	<head>
-		<title>Harvard-Radcliffe Asian American Christian Fellowship</title>
-		<link rel="stylesheet" href="CSS/style2.css"> 
-		<link href='http://fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Antic' rel='stylesheet' type='text/css'>
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-		<script src="scripts/jquery-1.2.1.min.js" type="text/javascript"></script>
-		<script src="scripts/menu.js" type="text/javascript"></script>
-		<script src="scripts/jquery.cookie.js" type="text/javascript"></script>
+		<title>HRAACF: Vision</title>
+		<?php include_once("includes/header.php");?> 
 	</head> 
 		<body> 
 			<div id="top"> 
