06fbec5be1bd9483b19fb8a3c6ce5d41612d9c07
Matthew Tung <matthew.tung.1@gmail.com>
2011-12-07 02:41:41 -0500

    Help Judy pull?

--- Judy/removemembers.php
+++ Judy/removemembers.php
@@ -11,49 +11,54 @@
 	  redirect("index.php");
 	
 	// find all unapproved members
-	$sql = "SELECT * FROM users WHERE approved = 1 AND id != 0";
-	$result = mysql_query($sql);
+	$sql = "SELECT * FROM users WHERE approved = 0 AND id != 0";
+	$unapproved = mysql_query($sql);
 
-	
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>HRAACF: Remove Member</title>
-    <?php include_once("includes/header.php");?> 
-  </head>
 
-	<body>
-		<?php include_once("identify.php");?> 
+<html>
+	 <head>
+   		<title>HRAACF: Approve Members</title>
+		<?php include_once("includes/header.php");?> 
+	</head>
+	<body> 
 		<div id="wrapper">  
-			<div id="left-side">
+		
+			<div id="left-side"> 
 			   <?php include_once('includes/navbar.php');?> 
 			   <?php include_once('includes/footer.php');?>
 			</div> 
 			
 			<div id="center"> 
-			<div class="useroptions" id="removemembers"> 
-			  <form action="approvemembers2.php" method="post">
-				  <table cellpadding="5">
-					<tr>
-					  <td align=left>Name</td>
-					  <td align=left>Email</td>
-					  <td align=left>Approve</td>
-					</tr>
-					<? while($unapproved = mysql_fetch_array($result)):?>
-					<tr>
-					  <td align=left><? echo $unapproved["name"] ?></td>
-					  <td align=left><? echo $unapproved["email"] ?></td>
-					  <td> <input name="<?echo $unapproved["id"]?>" type="checkbox"></td>
-					</tr>
-					<?endwhile;?>
-				   </table>
-				   <br><br>
-				   <input type="submit" value="Approve!">
-			  </form>
+				<div class="useroptions" id="approvemembers"> 
+				  <form action="removemembers2.php" method="post">
+					  <table cellpadding="8">
+						<tr>
+						  <th align=left>Username</th>
+						  <th align=left>Name</th>
+						  <th align=left>Email</th>
+						  <th align=left>Approve?</th>
+						</tr>
+						<? while($member = mysql_fetch_array($unapproved)):?>
+						<tr>
+						  <td align=left><? echo $member["username"] ?></td>
+						  <td align=left><? echo $member["name"] ?></td>
+						  <td align=left><? echo $member["email"] ?></td>
+						  <td> <input name="<?echo $member["id"]?>" type="checkbox"></td>
+						</tr>
+						<? endwhile; ?>
+					  </table>
+					  <br><br>
+					  <input type="submit" value="Approve!">
+				  </form>
+				</div>
 			</div>
-    		</div>
-    	</div> 	
-	</body>
+			
+			<div id="right-side"> 
+				<?php include_once('includes/usernav.php');?> 
+			</div>
+		</div> 
+	</body> 
 </html>
