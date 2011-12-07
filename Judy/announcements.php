3552178c0339bcdf1975c286f6059bbfe61224ab
Judy Park <judypark@judy-parks-macbook-pro.local>
2011-12-06 22:42:46 -0500

    lots of changes...

---
+++ Judy/announcements.php
@@ -0,0 +1,36 @@
+<? 
+
+require_once("includes/common.php"); 
+
+?> 
+
+<!DOCTYPE html>
+<html>	
+	<head>
+		<title>HRAACF: Announcements</title>
+		<?php include_once("includes/header.php");?> 
+	</head> 
+	
+	<body> 
+		<?php include_once("identify.php");?> 
+		<div id="wrapper">  
+			<div id="left-side"> 
+			   <?php include_once('includes/navbar.php');?> 
+			   <?php include_once('includes/footer.php');?>
+			</div> 
+			
+			<div id="center"> 
+			<div class="content" id="vision"> 
+				<h1>Announcements</h1> 
+				<h2>Winter Retreat</h2>
+				<p>Get pumped for Winter Retreat, which will be February 3-5 this year. More details on registration and packing lists to come. Frosh men, start recruiting your brethren for the special winter retreat activity!</p> 
+			</div>
+			</div>
+			
+			<div id="right-side"> 
+				<?php include_once('includes/usernav.php');?> 
+			</div>
+		</div> 
+   </body> 
+</html>
+		
---
+++ Judy/resources.php
@@ -0,0 +1,37 @@
+<? 
+
+require_once("includes/common.php"); 
+
+?> 
+
+<!DOCTYPE html>
+<html>	
+	<head>
+		<title>HRAACF: Resources</title>
+		<?php include_once("includes/header.php");?> 
+	</head> 
+	
+	<body> 
+		<?php include_once("identify.php");?> 
+		<div id="wrapper">  
+			<div id="left-side"> 
+			   <?php include_once('includes/navbar.php');?> 
+			   <?php include_once('includes/footer.php');?>
+			</div> 
+			
+			<div id="center"> 
+			<div class="content" id="resources"> 
+				<h1>Resources</h1> 
+				<h2>Read free apologetics <a href="http://truthbomb.blogspot.com/2009/07/free-apologetics-e-book-library.html" style='color: #57aa70; text-decoration: none'> e-books.</a></h2>
+				<h2>Find a <a href="http://hraacf.org/web-downloads/Church-List-0809.pdf" style='color: #57aa70; text-decoration: none'>church </a>in the Boston area.</h2> 
+				<h2>Listen to <a href="http://studio.graceabundant.com/albums/2008-HisKingdomCome/" style='color: #57aa70; text-decoration: none'>worship music</a> produced by our own AACF members.</h2> 
+			</div>
+			</div>
+			
+			<div id="right-side"> 
+				<?php include_once('includes/usernav.php');?> 
+			</div>
+		</div> 
+   </body> 
+</html>
+	
