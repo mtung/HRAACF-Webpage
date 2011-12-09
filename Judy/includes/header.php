abf9d491776cd801de98fb8e97c62a283f119461
Matthew Tung <matthew.tung.1@gmail.com>
2011-12-08 19:19:58 -0500

    Old Version

--- Judy/includes/header.php
+++ Judy/includes/header.php
@@ -3,4 +3,4 @@
 <link href='http://fonts.googleapis.com/css?family=Lato:300|Raleway:100' rel='stylesheet' type='text/css'>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
 <script src="scripts/menu.js" type="text/javascript"></script>
-
+<script src="scripts/jquery.cookie.js" type="text/javascript"></script>
